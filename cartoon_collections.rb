6e0bfe669a037d39028443047d2d2ea2fdeccb8d
Haley Grassi <teacherhaleymarie@gmail.com>
2020-08-30 14:22:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,9 +6,9 @@ def summon_captain_planet(array)
    array.collect { |word| word.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def long_planeteer_calls(array)
+  
+  
 
 def find_the_cheese# code an argument here
   # the array below is here to help
